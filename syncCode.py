f0184cc2343b52a650831fd1fad4b1bcacc4e8dc
scott-cao <499055602@qq.com>
2019-04-27 21:24:34 +0800

    [add] 添加可以sync 当前用户所有工程，并且不限制本工具的位置

--- syncCode.py
+++ syncCode.py
@@ -16,39 +16,75 @@ def Usage():
 	print "========================================================================"
 	time.sleep(1)
 
+class StdInput(object):
+
+	def __input__(self):
+		try:
+			aswan=sys.stdin.readline().strip()
+			return aswan
+		except:
+			return
+	def getInput(self):
+		signal.signal(signal.SIGALRM, self.__input__)
+		signal.alarm(TIMEOUT)
+		aswan=self.__input__()
+		if aswan is None:
+			return DEFEAULTINPUT
+		else:
+			return aswan
+
+class GetProjectClass(object):
+	Home=''
+	projectList=[]
+	stdinput=''
+
+	def __init__(self):
+		self.stdinput=StdInput()
+		self.Home=os.path.expanduser('~')#env=os.environ['HOME'] for windows!!!
+		print "Home is:"+self.Home
+
+	def __ConstructProjectList__(self,path):
+		#os.chdir(self.Home)
+		Dircots=os.listdir(path)
+		for direct in Dircots:
+			if direct == ".repo":
+				self.projectList.append(path)
+				return
+			elif os.path.isdir(direct):#为文件夹,继续遍历
+				self.__ConstructProjectList__(path+"/"+direct)
+			else:#为文件,遍历停止
+				continue
+
+	def SyncProjectList(self):
+		os.chdir(self.Home)
+		self.__ConstructProjectList__(self.Home)
+		print "含有.repo工程的工程文件夹有以下: "
+		for project in self.projectList:
+			GetCode=GetCodeClass(project);
+			print "工程: "+project+"  将更新"+str(TIMEOUT)+"S后默认sync"
+			aswan=self.stdinput.getInput()
+			if re.search("y",aswan,re.IGNORECASE):
+				GetCode.Sync()
+			else:
+				print "放弃"+project+"工程sync"
+			
+
 class GetCodeClass(object):
     ListGitPath=[]
     ListIgnorProject=[]
+    Home=""
     LocalPath=""
-    prePath=""
     syncerrorname="syncodeerror.log"
     
-
-    def __init__(self):
-    	self.LocalPath=os.getcwd()
-    	self.prePath=self.LocalPath+"/"
+    def __init__(self,path):
+    	self.LocalPath=path
+    	self.stdinput=StdInput()
 
     def __doConflict__(self,proname):
     	os.chdir(self.LocalPath+"/"+proname);
     	os.system("git checkout .")
     	os.system("git clean -xdf")
     	os.system("git pull")
-
-    def __input__(self):
-    	try:
-    		aswan=sys.stdin.readline().strip()
-    		return aswan
-    	except:
-    		return
-
-    def __getInput__(self):
-    	signal.signal(signal.SIGALRM, self.__input__)
-    	signal.alarm(TIMEOUT)
-    	aswan=self.__input__()
-    	if aswan is None:
-    		return DEFEAULTINPUT
-    	else:
-    		return aswan
 
     def __CreateFetchingErrorProject__(self):
     	os.chdir(self.LocalPath)
@@ -61,12 +97,12 @@ class GetCodeClass(object):
     def __conflictProject__(self,throwaway):
     	print "开始处理所有冲突的project!"
     	for projectname in self.ListGitPath:
-    		if re.search("yes",throwaway,re.IGNORECASE):
+    		if re.search("y",throwaway,re.IGNORECASE):
     			print "自动清理project: "+projectname
     		else:
     			print projectname+" 是否丢弃?留意10S后默认清理"
-    			aswan=self__getInput__()
-    			if re.search("yes",aswan,re.IGNORECASE):
+    			aswan=self.stdinput.getInput()
+    			if re.search("y",aswan,re.IGNORECASE):
     				print "清理project: "+projectname
     			else:
     				print "忽略project: "+projectname
@@ -79,12 +115,11 @@ class GetCodeClass(object):
     		for ignorename in self.ListIgnorProject:
     			print ignorename
 
-    def repoSyncCode(self):
-    	os.chdir(self.LocalPath)
+    def __repoSyncCode__(self):
     	os.system("repo sync 2>%s" %(self.syncerrorname))
     	self.__CreateFetchingErrorProject__()
 
-    def ResetconflictProject(self):
+    def __ResetconflictProject__(self):
     	if len(self.ListGitPath) == 0:
     		print "没有project有冲突,sync code 成功!!!"
     		return
@@ -92,13 +127,21 @@ class GetCodeClass(object):
     	for confictpath in self.ListGitPath:
     		print confictpath.lstrip()
     	print "是否全部自动全部丢弃? 留意10S后默认清理"
-    	aswan=self.__getInput__()
+    	aswan=self.stdinput.getInput()
     	self.__conflictProject__(aswan)
+
+    def Sync(self):
+    	print "开始更新................"
+    	os.chdir(self.LocalPath)
+    	self.__repoSyncCode__()
+    	self.__ResetconflictProject__()
+    	if os.path.exists(self.syncerrorname):
+    		os.remove(self.LocalPath+"/"+self.syncerrorname)
+    	print "更新完成................"
 
 if __name__ == '__main__':
 	reload(sys);
 	sys.setdefaultencoding('utf-8');
 	Usage()
-	GetCode=GetCodeClass();
-	GetCode.repoSyncCode();
-	GetCode.ResetconflictProject();
+	Project=GetProjectClass()
+	Project.SyncProjectList()
